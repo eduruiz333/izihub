2623c8e956b121f023d90e5adf31d7043e9383a7
Marcos Ruiz <marcos.ruiz@certisign.com.br>
2023-02-01 13:16:01 -0300

    componente de leia mais aplicado na LP

--- assets/js/izihub.js
+++ assets/js/izihub.js
@@ -1,3 +1,30 @@
+jQuery("#carousel_01").owlCarousel({
+    autoplay: false,
+    lazyLoad: true,
+    loop: false,
+    margin:10,
+    responsiveClass: true,
+    autoHeight: false,
+    autoplayTimeout: 7000,
+    smartSpeed: 800,
+    nav: true,
+    dots: false,
+    responsive: {
+        0: {
+            items: 2
+        },
+        600: {
+            items: 3
+        },
+        1024: {
+            items: 3
+        },
+        1366: {
+            items: 3
+        }
+    }
+})
+
 jQuery("#carousel_02").owlCarousel({
     autoplay: false,
     lazyLoad: false,
@@ -22,23 +49,44 @@ jQuery("#carousel_02").owlCarousel({
             items: 3
         }
     }
-});
+})
+
+jQuery("#carousel_03").owlCarousel({
+    autoplay: false,
+    lazyLoad: true,
+    loop: true,
+    responsiveClass: true,
+    autoHeight: false,
+    autoplayTimeout: 7000,
+    smartSpeed: 800,
+    nav: true,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 2
+        },
+        1024: {
+            items: 3
+        },
+        1366: {
+            items: 3
+        }
+    }
+})
 
 
-const getContentHeight = document.querySelectorAll('.card-body')
+const getContentHeight = document.querySelectorAll('.card-height')
 const createButton = document.querySelectorAll('.create-btn-expand')
-
-let btn = `<button class="btn btn-primary"></button>`
-
+const btn = `<button class="btn btn-primary"></button>`
 
 for (let i = 0; i < getContentHeight.length; i++) {
     const alturas = getContentHeight[i].offsetHeight
 
     if (alturas > 165) {
-        
         getContentHeight[i].classList.add('crop');
         createButton[i].innerHTML = btn
-        
         createButton[i].addEventListener("click", addButton);
 
         function addButton() {
