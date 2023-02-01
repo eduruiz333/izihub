439c333b1d802af7cecd1d01892e07acfd42f7aa
Marcos Ruiz <marcos.ruiz@certisign.com.br>
2023-02-01 15:58:07 -0300

    Finalização do primeiro leia mais

--- assets/js/izihub.js
+++ assets/js/izihub.js
@@ -28,13 +28,14 @@ jQuery("#carousel_01").owlCarousel({
 jQuery("#carousel_02").owlCarousel({
     autoplay: false,
     lazyLoad: false,
-    loop: false,
+    loop: true,
     margin: 10,
     responsiveClass: true,
     autoHeight: false,
     autoplayTimeout: 7000,
     smartSpeed: 400,
     nav: true,
+    dots: false,
     responsive: {
         0: {
             items: 1
@@ -54,15 +55,17 @@ jQuery("#carousel_02").owlCarousel({
 jQuery("#carousel_03").owlCarousel({
     autoplay: false,
     lazyLoad: true,
+    margin:10,
     loop: true,
     responsiveClass: true,
     autoHeight: false,
     autoplayTimeout: 7000,
     smartSpeed: 800,
     nav: true,
+    dots: false,
     responsive: {
         0: {
-            items: 1
+            items: 2
         },
         600: {
             items: 2
@@ -80,12 +83,16 @@ jQuery("#carousel_03").owlCarousel({
 const getContentHeight = document.querySelectorAll('.card-height')
 const createButton = document.querySelectorAll('.create-btn-expand')
 const btn = `<button class="btn btn-primary"></button>`
+
+
 for (let i = 0; i < getContentHeight.length; i++) {
     const alturas = getContentHeight[i].offsetHeight
+
     if (alturas > 165) {
         getContentHeight[i].classList.add('crop')
         createButton[i].innerHTML = btn
         createButton[i].addEventListener("click", addButton)
+
         function addButton() {
             getContentHeight[i].classList.toggle('crop')
             createButton[i].classList.toggle('less')
